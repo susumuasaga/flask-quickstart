d6cc7d7c8af6fc6de062f7a32c6d442285d46a00
Susumu Asaga <susumu.asaga@gmail.com>
2020-11-27 14:09:49 -0300

    Compatibilizar hello.py com Python 2.7

--- hello.py
+++ hello.py
@@ -8,7 +8,7 @@ port = int(os.getenv("PORT", 8080))
 
 @app.route('/')
 def hello_world():
-    return f'Hello World! I am running on port {port}'
+    return 'Hello World! I am running on port ' + str(port)
 
 
 if __name__ == '__main__':
