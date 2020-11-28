6d930bccd6d30f68b9daea1ec0ba8f16970fe887
Susumu Asaga <susumu.asaga@gmail.com>
2020-11-28 15:31:04 -0300

    Executar o servidor web

--- hello.py
+++ hello.py
@@ -3,13 +3,12 @@ import os
 
 app = Flask(__name__)
 
-port = int(os.getenv("PORT", 8080))
-
 
 @app.route('/')
 def hello_world():
-    return f'Hello World! I am running on port {port}'
+    return f'Hello World! I am running on port {port}.'
 
 
 if __name__ == '__main__':
+    port = int(os.getenv("PORT", 8080))
     app.run('0.0.0.0', port)
